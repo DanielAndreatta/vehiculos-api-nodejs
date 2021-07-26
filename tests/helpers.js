28eb0413123429b607206606fa73c3e42504958d
Daniel <danielandreatta2@gmail.com>
2021-07-26 20:13:47 -0300

    añado tests GET,

---
+++ tests/helpers.js
@@ -0,0 +1,56 @@
+const {app} = require('../index')
+const supertest = require('supertest')
+const Vehiculo = require('../models/Vehiculo')
+
+const api = supertest(app)
+
+const initialVehiculos = [
+    {
+    vehiculo: "Integra GS 1.8",
+    marca: "Acura",
+    ano: 1996,
+    descripcion: "descripción del vehículo",
+    vendido: false,
+    created: "2021-07-26T00:49:53.603Z",
+    updated: "2021-07-26T18:16:46.760Z"
+    },
+    {
+    vehiculo: "NSX3.0",
+    marca: "Acura",
+    ano: 2000,
+    descripcion: "descripción del vehículo",
+    vendido: true,
+    created: "2021-07-26T00:49:57.412Z",
+    updated: "2021-07-26T18:19:00.114Z"
+    },
+    {
+    vehiculo: "Punto BLACKMOTION 1.8 Flex 16V 5p.",
+    marca: "Fiat",
+    ano: 1996,
+    descripcion: "descripción del vehículo",
+    vendido: false,
+    created: "2021-07-26T18:26:49.290Z",
+    updated: null
+    }
+]
+
+const getVehiculos = async () => {
+    const vehiculosDB = await Vehiculo.find({})
+    return vehiculosDB.map( vehiculo => vehiculo.toJSON())
+}
+
+const getAllFromVehiculos = async () => {
+
+    const response = await api.get('/vehiculos')
+    return {
+        marca: response.body.map( marca => marca.marca),
+        vehiculo: response.body.map( vehiculo => vehiculo.vehiculo),
+        ano: response.body.map( ano => ano.ano),
+        descripcion: response.body.map( descripcion => descripcion.descripcion),
+        vendido: response.body.map( vendido => vendido.vendido),
+        response
+    }
+}
+
+
+module.exports = {api, initialVehiculos, getAllFromVehiculos, getVehiculos}
---
+++ tests/vehiculo.test.js
@@ -0,0 +1,154 @@
+const moongose = require('mongoose')
+const {server} = require('../index')
+const Vehiculo = require('../models/Vehiculo')
+
+const {api, initialVehiculos, getAllFromVehiculos, getVehiculos} = require('./helpers')
+
+
+beforeEach( async () => {
+
+    await Vehiculo.deleteMany({})
+
+    // secuencial, guarda ordenadamente
+    for (const vehiculo of initialVehiculos){
+        const vehiculosObject = new Vehiculo(vehiculo)
+        await vehiculosObject.save()
+    }
+})
+
+
+
+describe('test a GET(/vehiculos)', () => {
+
+    test('los vehiculos se devuelven en json', async () => {
+        await api
+            .get('/vehiculos')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('queremos ver tres vehiculos', async () => {
+        const response = await api.get('/vehiculos')
+        expect(response.body).toHaveLength(initialVehiculos.length)
+    })
+
+    test('algun vehiculo contiene la marca "Fiat"', async () => {
+        const {marca} = await getAllFromVehiculos()
+    
+        expect(marca).toContain('Fiat')
+    })
+
+    test('algun vehiculo contiene el nombre  "Punto BLACKMOTION 1.8 Flex 16V 5p."', async () => {
+        const {vehiculo} = await getAllFromVehiculos()
+    
+        expect(vehiculo).toContain('Punto BLACKMOTION 1.8 Flex 16V 5p.')
+    })
+
+    test('algun vehiculo contiene la descripcion  "descripción del vehículo"', async () => {
+        const {descripcion} = await getAllFromVehiculos()
+    
+        expect(descripcion).toContain('descripción del vehículo')
+    })
+
+    test('algun vehiculo contiene el año "2000"', async () => {
+        const {ano} = await getAllFromVehiculos()
+    
+        expect(ano).toContain(2000)
+    })
+
+    test('algun vehiculo contiene vendido = "true"', async () => {
+        const {vendido} = await getAllFromVehiculos()
+    
+        expect(vendido).toContain(true)
+    })
+    
+})
+
+describe('test a POST(/vehiculos)', () => {
+
+    test('añadir un nuevo vehiculo valido', async () => {
+        const newVehiculo = {
+                vehiculo: "Twingo Pack 1.0 16V 70cv",
+                marca: "Renault",
+                ano: 1998,
+                descripcion: "descripción del vehículo",
+                vendido: false,
+                created: new Date() ,
+                updated: new Date()   
+        }
+        
+    
+        await api
+            .post('/vehiculos')
+            .send(newVehiculo)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const {vehiculo, response} = await getAllFromVehiculos()
+    
+        expect(response.body).toHaveLength(initialVehiculos.length+1)
+    
+        expect(vehiculo).toContain(newVehiculo.vehiculo)
+    })
+
+    test('no se puede añadir un vehiculo sin modelo', async () => {
+        const newVehiculo = {
+            marca: "Renault",
+            ano: 1998,
+            descripcion: "descripción del vehículo",
+            vendido: false,
+            created: new Date() ,
+            updated: new Date()  
+        }
+    
+        await api
+            .post('/vehiculos')
+            .send(newVehiculo)
+            .expect(400)
+    
+        const response = await api.get('/vehiculos')
+    
+        expect(response.body).toHaveLength(initialVehiculos.length)
+    })
+})
+
+
+describe('test a GET(/vehiculos/:id)', () => {
+
+    test('debe borrarse un vehiculo', async () => {
+        const {response: firstResponse} = await getAllFromVehiculos()
+        const {body:vehiculos} = firstResponse
+        const vehiculoToDelete = vehiculos[0]
+    
+        await api
+            .delete(`/vehiculos/${vehiculoToDelete.id}`)
+            .expect(204)
+        
+        
+        const {vehiculo, response: secondResponse} = await getAllFromVehiculos()
+        
+        expect(secondResponse.body).toHaveLength(initialVehiculos.length-1)
+    
+        expect(vehiculo).not.toContain(vehiculoToDelete.vehiculo)
+    })
+    
+    
+    test('el vehiculo no debe borrarse porque la id no existe', async () => {
+        await api
+            .delete(`/vehiculos/123135`)
+            .expect(400)
+        
+        
+        const {response} = await getAllFromVehiculos()
+        
+        expect(response.body).toHaveLength(initialVehiculos.length)
+    })
+})
+
+
+
+
+afterAll( () => {
+    moongose.connection.close();
+    server.close();
+})
